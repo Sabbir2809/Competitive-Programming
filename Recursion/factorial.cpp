9df331034f5aa29cfb9976c3256047d98e18a58a
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-11-24 19:47:44 +0600

    Recursion- Factorial Number1

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,29 @@
+/*
+    Author : Sabbir Hossain
+    Problem_Name : Factorial Number using Recursion
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(long long n){
+    if(n == 1){ // Base Condition
+        return 1;
+    }
+    return n * factorial(n - 1); // Hypothesis Design, Induction
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+/*  
+    Recursion: Hypothesis Design, Induction (print), Base Condition 
+*/  
+    long long n;
+    cin >> n;
+
+    long long ans = factorial(n);
+    cout << ans << "\n";
+
+return 0;
+}
