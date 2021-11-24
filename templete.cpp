923c554030717c433d62c408140d5c57fc16623c
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-11-24 19:50:01 +0600

    Competitive Programming C++ Code Templete

--- templete.cpp
+++ templete.cpp
@@ -1,13 +1,9 @@
 /*
     Author : Sabbir Hossain
-    Problem_Name : Competitive Programming Templete
+    Problem_Name : Competitive Programming Code Templete
 */
 #include<bits/stdc++.h>
 using namespace std;
-
-const double PI = acos(-1.0);
-typedef long long ll;
-typedef unsigned long long ull;
 
 int main(){
     ios_base::sync_with_stdio(false);
