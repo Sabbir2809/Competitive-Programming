6c6712102feb066d5646e2a2f0d212dc7788abcc
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-11-24 21:08:56 +0600

    Recursion- Sum of Digits of a Number

--- Recursion/sumofDigits.cpp
+++ Recursion/sumofDigits.cpp
@@ -5,19 +5,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sum_digits(int n){
+int sumOfDigit(int n){
     if(n == 0){
         return 0;
     }
     int last_digit = n % 10;
     int remaining = n / 10;
-    cout << remaining << " " << last_digit << "\n";
-    return sum_digits(remaining) + last_digit;
+
+    cout << remaining << " " << last_digit << "\n"; 
+    return sumOfDigit(remaining) + last_digit;
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    cout << sum_digits(12143);
+    long long n;
+    cin >> n;
+
+    int result = sumOfDigit(n);
+    cout << result << "\n";
+    
 return 0;
 }
