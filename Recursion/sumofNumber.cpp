16fb9fd1814badbe9b6515084fb186ada25d0e6c
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-11-24 21:12:53 +0600

    Recursion- Sum of Number from 1 to n

--- Recursion/sumofNumber.cpp
+++ Recursion/sumofNumber.cpp
@@ -4,20 +4,24 @@
 */
 #include<bits/stdc++.h>
 using namespace std;
-// Very Important: base case, otherwise never stop
-// Trust
-// return 1 + 2 + 3 + ... + (n-1) + n
-int sum_n(int n){
+
+int sumOfNumber(int n){
     if(n == 0){
         return 0;
     }
-    int left_part = sum_n(n-1);
+    int left_part = sumOfNumber(n-1);
     return left_part + n;
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    cout << sum_n(10);
+    int n;
+    cin >> n;
+
+    cout << sumOfNumber(n) << "\n";
+    
 return 0;
 }
