66a4ef004de38c316d057493fd9df54fda0cdf3b
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-12-02 11:39:08 +0600

    C++ Code Templete

--- templete.cpp
+++ templete.cpp
@@ -1,16 +1,27 @@
 /*
-    Author : Sabbir Hossain
-    Problem_Name : Competitive Programming Code Templete
+    Author: Sabbir Hossain
+    Problem_Name: 
 */
 #include<bits/stdc++.h>
 using namespace std;
+
+void solveProblem(){
+    
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    // Code Here    
+    int test_case;
+    cin >> test_case;
+
+    // test_case = 1;
+
+    while(test_case--){
+        solveProblem();
+    }
 
 return 0;
 }
