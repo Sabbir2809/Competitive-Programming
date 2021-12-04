22f925a2279a08382f7d6c6d58363d69c4ddb5d2
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-12-04 20:42:45 +0600

    Templete C++

--- templete.cpp
+++ templete.cpp
@@ -5,23 +5,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solveProblem(){
-    
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int test_case;
-    cin >> test_case;
+    int tc;
+    cin >> tc;
 
-    // test_case = 1;
-
-    while(test_case--){
-        solveProblem();
-    }
-
+    
+    
 return 0;
 }
