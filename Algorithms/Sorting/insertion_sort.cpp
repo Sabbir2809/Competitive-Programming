798252a5634b0a76cf0a1e16e3a0c4d300b885c4
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-12-05 13:47:39 +0600

    Sorting Algorithm - Insertion Sort

---
+++ Algorithms/Sorting/insertion_sort.cpp
@@ -0,0 +1,45 @@
+/*
+    Author: Sabbir Hossain
+    Problem_Name: Insertion Sort
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int i, value, hole;
+void sort(int n,int a[]){
+    for(i = 1; i < n; i++){ //n
+        value = a[i];
+        hole = i;
+
+        while(hole > 0 && a[hole-1] > value){ //n
+            a[hole] = a[hole-1];
+            hole--;
+        }
+        a[hole] = value;
+    }
+    for(i = 0; i < n; i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");  
+}
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+    for(i = 0; i < n; i++){
+        scanf("%d",&a[i]);
+    }
+
+    sort(n,a);
+
+    if (n % 2 == 0){
+        printf("%d\n",a[(n/2)]);
+    }
+    else{
+        printf("%d\n",a[((n+1)/2)-1]);
+    }
+return 0;
+}
