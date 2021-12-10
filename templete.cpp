c732122b026b97d10b144eac077caa00b24358eb
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2021-12-10 10:28:25 +0600

    C++ Code Templete

--- templete.cpp
+++ templete.cpp
@@ -5,15 +5,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll; // 10^12
+typedef long long int lli; // 10^18
+typedef long double ld;
+typedef unsigned long long ull;
+
+const double PI = acos(-1.0);
+
+#define case(i) cout << "Case #" << i << ": ";
+#define endl '\n';
+
+void solve(){
+    
+}
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "w", stderr);
 
-    int tc;
-    cin >> tc;
+    solve();
 
-    
-    
 return 0;
 }
